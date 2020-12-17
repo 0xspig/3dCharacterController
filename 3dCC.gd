49949cc5b2b42df7c8b3be3e8c1537100790dbed
0xspig <private@faggot.biz>
2020-12-17 18:00:06 -0500

    Added speed accumulation to bhops.

--- 3dCC.gd
+++ 3dCC.gd
@@ -2,8 +2,8 @@ extends KinematicBody
 
 enum State {IDLE, RUN, JUMP, FALL}
 
-const JUMP_SPEED = 5
-const JUMP_FRAMES = 5
+const JUMP_SPEED = 7
+const JUMP_FRAMES = 1
 const HOP_FRAMES = 3
 
 export var mouse_y_sens = .1
@@ -14,6 +14,8 @@ export var gravity = -10
 export var friction = 1.15
 export var max_climb_angle = .6
 export var angle_of_freedom = 80
+export var boost_accumulation_speed = 1
+export var max_boost_multiplier = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -40,12 +42,14 @@ func _input(event):
 var inbetween = false
 func _on_tween_all_completed():
 	inbetween = false
+	crouch_floor = false
 
 
 var state = State.FALL
 var on_floor = false
 var frames = 0
 var crouching = false
+var crouch_floor = false #true if started crouching on the floor
 var input_dir = Vector3(0, 0, 0)
 func _process_input(delta):
 	# Toggle mouse capture
@@ -62,11 +66,14 @@ func _process_input(delta):
 	
 	# Crouch
 	if Input.is_action_just_pressed("crouch"):
+		if on_floor:
+			crouch_floor = true
 		crouching = true
 		$Tween.interpolate_property($LowerCollider, "translation", 
 				Vector3(0, -.25, 0), Vector3(0,.25, 0), .1, Tween.TRANS_LINEAR)
 		$Tween.start()
 		inbetween = true
+		
 	if Input.is_action_just_released("crouch"):
 		crouching = false
 		$Tween.interpolate_property($LowerCollider, "translation", 
@@ -83,6 +90,7 @@ func _process_input(delta):
 var collision : KinematicCollision  # Stores the collision from move_and_collide
 var velocity := Vector3(0, 0, 0)
 var rotation_buf = rotation  # used to calculate rotation delta for air strafing
+var turn_boost = 1
 func _process_movement(delta):
 	# state management
 	if !collision:
@@ -98,9 +106,10 @@ func _process_movement(delta):
 			on_floor = true
 			if input_dir.length() > .1 && (frames > JUMP_FRAMES+HOP_FRAMES || frames == 0):
 				state = State.RUN
+				turn_boost = 1
 			else:
 				state = State.IDLE
-
+	
 	#jump state
 	if state == State.JUMP && frames < JUMP_FRAMES:
 		velocity.y = JUMP_SPEED
@@ -110,29 +119,24 @@ func _process_movement(delta):
 
 	#fall state
 	if state == State.FALL:
+		if inbetween && crouching && crouch_floor:
+			velocity.y = gravity;
 		if velocity.y > gravity:
 			velocity.y += gravity * delta * 4
 	
 	#run state
 	if state == State.RUN:
-		if !crouching:
-			velocity += input_dir.rotated(Vector3(0, 1, 0), rotation.y) * acceleration
-			if Vector2(velocity.x, velocity.z).length() > move_speed:
-				velocity = velocity.normalized() * move_speed
-			velocity.y = ((Vector3(velocity.x, 0, velocity.z).dot(collision.normal)) * -1)
-			velocity.y -= 1 + (1+int(velocity.y < 0) * .3)
-		else:
-			velocity += input_dir.rotated(Vector3(0, 1, 0), rotation.y) * acceleration
-			if Vector2(velocity.x, velocity.z).length() > move_speed/2:
-				velocity = velocity.normalized() * move_speed/2
-			velocity.y = ((Vector3(velocity.x, 0, velocity.z).dot(collision.normal)) * -1)
-			velocity.y -= 1 + (1+int(velocity.y < 0) * .3)
-			
+		velocity += input_dir.rotated(Vector3(0, 1, 0), rotation.y) * acceleration
+		if Vector2(velocity.x, velocity.z).length() > (move_speed/2 if crouching else move_speed):
+			velocity = velocity.normalized() * (move_speed/2 if crouching else move_speed)
+		velocity.y = ((Vector3(velocity.x, 0, velocity.z).dot(collision.normal)) * -1)
+		velocity.y -= 1 + (1+int(velocity.y < 0) * .3)
 
 	#idle state
 	if state == State.IDLE && frames < HOP_FRAMES + JUMP_FRAMES:
 		frames += 1 * delta * 60
 	elif state == State.IDLE:
+		turn_boost = 1
 		if velocity.length() > .5:
 			velocity /= friction
 			velocity.y = ((Vector3(velocity.x, 0, velocity.z).dot(collision.normal)) * -1) - .0001
@@ -142,9 +146,11 @@ func _process_movement(delta):
 		#x axis movement
 		var rotation_d = rotation - rotation_buf
 		if input_dir.x > .1 && rotation_d.y < 0:
+			velocity = velocity.rotated(Vector3.UP, rotation_d.y )
+			turn_boost += boost_accumulation_speed * delta 
+		elif input_dir.x < -.1 && rotation_d.y > 0:
 			velocity = velocity.rotated(Vector3.UP, rotation_d.y ) 
-		if input_dir.x < -.1 && rotation_d.y > 0:
-			velocity = velocity.rotated(Vector3.UP, rotation_d.y ) 
+			turn_boost += boost_accumulation_speed * delta 
 		
 		if abs(input_dir.x) < .1 && on_floor:
 			#z axis movement
@@ -155,19 +161,21 @@ func _process_movement(delta):
 				var xy = Vector2(movement_vector.x , movement_vector.z).normalized()
 				velocity += Vector3(xy.x, 0, xy.y) * acceleration
 				
-			
+		turn_boost = clamp(turn_boost, 1, max_boost_multiplier)
 		rotation_buf = rotation
-			
+
 	#apply
 	if velocity.length() >= .5 || inbetween:
-		collision = move_and_collide(velocity * delta)
+		collision = move_and_collide(velocity * Vector3(turn_boost, 1, turn_boost) * delta)
 	else:
 		velocity = Vector3(0, velocity.y, 0)
 	if collision:
 		if Vector3.UP.dot(collision.normal) < .5:
 			velocity.y += delta * gravity
 			clamp(velocity.y, gravity, 9999)
-			velocity = velocity.slide(collision.normal)
+			velocity = velocity.slide(collision.normal).normalized() * velocity.length()
+		elif turn_boost > 1.01:
+			velocity = Vector3(velocity.x, velocity.y + ((Vector3(velocity.x, 0, velocity.z).dot(collision.normal)) * - 2) , velocity.z)
 		else:
 			velocity = velocity
 
